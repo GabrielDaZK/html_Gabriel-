ea7a0c9b2aa8f1226a8f1d480fbfe70f1d952b24
GabrielDaZK <gabriel.antunes.correa@escola.pr.gov.br>
2025-06-16 12:21:25 -0300

    Add files via upload

---
+++ exercicio9/script9.js
@@ -0,0 +1,13 @@
+let n1;
+let n2;
+
+function numero(){
+    n1 = window.prompt("Digite o 1° número");
+    n2 = window.prompt("Digite o 2° número");
+    if (n1 > n2){
+        alert(" o 1° número é maior");
+    } else {
+        alert("0 2° número é maior");
+    }
+     
+}
